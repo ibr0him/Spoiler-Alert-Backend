788650ac953a57f296a78829b985fcca64303c8d
ibr0him <ibrahimmedhat61@gmail.com>
2025-05-25 23:04:33 +0300

    Restructuring

---
+++ MoviesBackend/Entities/Movie.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MoviesBackend.Entities
+{
+    public class Movie
+    {
+      
+        public int id { get; set; }
+
+        public string Image { get; set; }
+        public string Title { get; set; }
+        public string Type { get; set; }
+        
+        [Column(TypeName = "varchar(3)")]
+        public string Rating { get; set; }
+    }
+}
